5b19052e046ff581ad722d26337a6e48f3a62a9d
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-04-13 22:00:41 +0900

    feat: use openapi typing

--- packages/k6/src/create-users.ts
+++ packages/k6/src/create-users.ts
@@ -6,6 +6,7 @@ import {
   randomString
   /* @ts-ignore */
 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js'
+import { CreateUserRequestBody } from './generated'
 
 export let options: Options = {
   vus: 5,
@@ -13,10 +14,11 @@ export let options: Options = {
 }
 
 export default () => {
-  const res = http.post(`http://localhost:3001/users`, {
+  const body: CreateUserRequestBody = {
     displayName: `Jon${randomString(1, 'aeiou')}s`,
     email: `user_${randomString(10)}@example.com`
-  })
+  }
+  const res = http.post(`http://localhost:3001/users`, body as any)
   check(res, {
     'status is 201': () => res.status === 201
   })
