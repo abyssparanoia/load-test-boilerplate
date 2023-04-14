6d2029c6a7f378044227205a9eff54983ea7e5df
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2023-04-14 15:46:13 +0900

    feat: impl report k6

--- packages/k6/src/create-and-get-users.ts
+++ packages/k6/src/create-and-get-users.ts
@@ -2,6 +2,8 @@ import { group, sleep } from 'k6'
 import { Options } from 'k6/options'
 import { createUserScript } from './scripts/create-user-script.js'
 import { getUserScript } from './scripts/get-user-scripts.js'
+/* @ts-ignore */
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js'
 
 export let options: Options = {
   vus: 5,
@@ -14,4 +16,10 @@ export default () => {
     getUserScript(createUserResult.user.id)
   })
   sleep(1)
+}
+
+export function handleSummary(data: any) {
+  return {
+    '../k6-report/src/create-and-get-users.html': htmlReport(data)
+  }
 }
